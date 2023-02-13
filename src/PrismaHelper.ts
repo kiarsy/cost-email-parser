2fb775c63e8696ba3b4b0afe44af166ceeb1654b
kiarsy <kiarsy@yahoo.com>
2023-02-13 16:59:51 +0400

    fix

--- src/PrismaHelper.ts
+++ src/PrismaHelper.ts
@@ -7,56 +7,61 @@ export class PrismaHelper
 
     async handle(event: EventType)
     {
-        let user = await this.prisma.userEmails.findFirst({ where: { email: getEmail(event.mail.from) } });
+        try{
+            let user = await this.prisma.userEmails.findFirst({ where: { email: getEmail(event.mail.from) } });
         
-        if (!user) {
-            console.log("User not found:", getEmail(event.mail.from));
-            return;
-        }
-        
-        let account = await this.prisma.account.findFirst({
-            where: {
-                bank: event.meta.bank,
-                account: String(event.meta.accountNumber),
-                userId: user.userId!
+            if (!user) {
+                console.log("User not found:", getEmail(event.mail.from));
+                return;
             }
-        });
-        
-        if (!account) {
-            account = await this.prisma.account.create({
-                data: {
+            
+            let account = await this.prisma.account.findFirst({
+                where: {
                     bank: event.meta.bank,
                     account: String(event.meta.accountNumber),
                     userId: user.userId!
                 }
             });
-        }
-        
-        try {
-            await this.prisma.cost.create({
-                data: {
-                    credit: event.record.credit,
-                    currency: event.record.currency,
-                    date: event.record.date,
-                    debit: event.record.debit,
-                    description: event.record.description,
-                    accountId: account.id
-                }
-            })
-            console.log("add one record");
-        }
-        catch (e: any) {
-            // console.error(e, 'Error creating character')
-        
-            // if (e instanceof Prisma.PrismaClientKnownRequestError) {
-            //     // P2022: Unique constraint failed
-            //     // Prisma error codes: https://www.prisma.io/docs/reference/api-reference/error-reference#error-codes
-            if (e.code === 'P2002') {
-                console.error('The Record already exists', e)
-                // throw new RedwoodError('The character already exists')
+            
+            if (!account) {
+                account = await this.prisma.account.create({
+                    data: {
+                        bank: event.meta.bank,
+                        account: String(event.meta.accountNumber),
+                        userId: user.userId!
+                    }
+                });
             }
-            // }
+            
+            try {
+                await this.prisma.cost.create({
+                    data: {
+                        credit: event.record.credit,
+                        currency: event.record.currency,
+                        date: event.record.date,
+                        debit: event.record.debit,
+                        description: event.record.description,
+                        accountId: account.id
+                    }
+                })
+                console.log("add one record");
+            }
+            catch (e: any) {
+                // console.error(e, 'Error creating character')
+                // if (e instanceof Prisma.PrismaClientKnownRequestError) {
+                //     // P2022: Unique constraint failed
+                //     // Prisma error codes: https://www.prisma.io/docs/reference/api-reference/error-reference#error-codes
+                if (e.code === 'P2002') {
+                    console.error('The Record already exists', e);
+                    // throw new RedwoodError('The character already exists')
+                }
+                // }
+            }
         }
+       catch(e:any)
+       {
+        console.error(e, 'Error while adding record');
+       }
         
     }
 }
